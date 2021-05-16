921373cbbc34dc3cc10754dfb322c7939da33134
K.T MOHAMMED SULAIM <ktmsulaim@gmail.com>
2021-05-16 14:38:37 +0530

    Added redirection to home route to login

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 })->name('welcome');
 
 Auth::routes(['register' => false]);
