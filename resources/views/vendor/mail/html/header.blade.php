ce18e0d614eb0a1211e90844d4bdf194b7cc6ce2
ktmsulaim <ktmsulaim@gmail.com>
2023-03-27 08:00:25 +0530

    Added logo to email template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ asset('img/icon.png') }}" class="logo" alt="Ahsan Logo">
-@else
-{{ $slot }}
-@endif
+    <img src="{{ asset('img/icon.png') }}" class="logo" alt="Ahsan Logo">
 </a>
 </td>
 </tr>
